77a8d6db239821d2903470bb9eb46c814d8ea50d
AlexV0102 <oleksandr.vasyliev04@gmail.com>
2024-10-15 15:34:57 +0300

    init commit

---
+++ Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace lab_4.Controllers.Commands
+{
+    public interface ICommand
+    {
+        public string Name { get; }
+        public async Task Execute(Update update) { }
+    }
+}
